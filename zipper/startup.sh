eb068aeb7821369e2ed6a6ff1eb2ad18f0105cbb
Johan Euphrosine <proppy@google.com>
2014-06-27 13:08:07 -0700

    zipper: switch to managed vms

--- zipper/startup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-# Startup script for to be run on the Google Cloud VM.
-
-git clone https://github.com/PolymerLabs/bowerzipper
-
-sudo docker build -t bowerzipper bowerzipper/
-sudo docker run -p 80:8080 bowerzipper &
